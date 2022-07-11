58f6656fe10220b6298ec1df3353a0a5ea88879f
Levi <levikeithmastrud@gmail.com>
2022-07-11 10:30:27 -0500

    trying to get three js to work

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,7 +4,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../docs/script.js'),
+    entry: path.resolve(__dirname, '../dist/script.js'),
     output:
     {
         filename: 'bundle.[contenthash].js',
@@ -19,7 +19,7 @@ module.exports = {
             ]
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../docs/index.html'),
+            template: path.resolve(__dirname, '../dist/index.html'),
             minify: true
         }),
         new MiniCSSExtractPlugin()
